9e0ac8e6dd177389f5c880af3d59ef772a1c96b2
SiboneyPalomino <37349502+SiboneyPalomino@users.noreply.github.com>
2018-10-22 16:18:33 -0500

    Add files via upload

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by siboney.rios on 10/22/18.
+//  Copyright © 2018 siboney.rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    // MARK: IBOutlet
+    @IBOutlet weak var imvDicee1: UIImageView!
+    @IBOutlet weak var imvDicee2: UIImageView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+     // MARK: IBAction
+    @IBAction func btnRoll(_ sender: Any)
+    {
+        
+    }
+    
+}
+
