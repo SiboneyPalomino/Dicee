dd6211a305191cb795d6925dc0c2f53d3b8aebf7
Siboney <siboney.palomino_93@hotmail.com>
2018-10-23 14:36:02 -0500

    updateDiceImages was added

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    // MARK: Properties
+    var randomIndex1: Int = 0
+    var randomIndex2: Int = 0
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     // MARK: IBOutlet
     @IBOutlet weak var imvDicee1: UIImageView!
     @IBOutlet weak var imvDicee2: UIImageView!
@@ -17,6 +22,7 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.updateDiceImages()
         
     }
 
@@ -24,11 +30,24 @@ class ViewController: UIViewController
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateDiceImages()
+    {
+        randomIndex1 = Int(arc4random_uniform(6))
+        randomIndex2 = Int(arc4random_uniform(6))
+      
+        self.imvDicee1.image = UIImage(named: diceArray[randomIndex1])
+        self.imvDicee2.image = UIImage(named: diceArray[randomIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        self.updateDiceImages()
+    }
 
      // MARK: IBAction
     @IBAction func btnRoll(_ sender: Any)
     {
-        
+       self.updateDiceImages()
     }
     
 }
